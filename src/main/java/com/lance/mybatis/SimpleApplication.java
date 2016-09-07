1abf18f3e9deeb99fd52ae78d15f996d57f7d980
mcyy <mcyy568@163.com>
2016-09-07 17:22:05 +0800

    升级spring boot版本 为1.4.0.RELEASE

--- src/main/java/com/lance/mybatis/SimpleApplication.java
+++ src/main/java/com/lance/mybatis/SimpleApplication.java
@@ -1,13 +1,45 @@
 package com.lance.mybatis;
 
+import javax.sql.DataSource;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 @SpringBootApplication
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.lance.mybatis.mapper")
 public class SimpleApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		LOGGER.info("Configuring Datasource");
+		return new DruidDataSource();
+	}
+
+	@Bean
+	public PlatformTransactionManager txManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 
 }
